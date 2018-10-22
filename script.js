a4a9b94c1a9749392b11499873d4f018a24450a7
Camboridesbikes <hattendorfcameron@gmail.com>
2018-10-21 19:35:24 -0700

    changed info in h3 to output usable css

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const body = document.getElementById("gradient");
 
 const change = () => {
   body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
-  css.innerHTML = `${color1.value} and ${color2.value}`;
+  css.innerHTML =  `linear-gradient(to right, ${color1.value}, ${color2.value})`;
 };
 
 const changeRand = (data) => {
@@ -15,7 +15,7 @@ const changeRand = (data) => {
   color1.value = '#' + data.colors[0].hex;
     color2.value = '#' + data.colors[1].hex;
   body.style.background = `linear-gradient(to right, #${data.colors[0].hex}, #${data.colors[1].hex})`;
-  css.innerHTML = `#${data.colors[0].hex} and #${data.colors[1].hex}`;
+  css.innerHTML = `linear-gradient(to right, #${data.colors[0].hex}, #${data.colors[1].hex})`;
 };
 
 const randomize = (event) => {
